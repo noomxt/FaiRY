d74d1363c63d3a76b0e91caf9404940c3511158b
Park Ji Su <parkjs1100@naver.com>
2025-11-27 20:15:09 +0900

    file import and def modify

--- fairy/text_emotion.py
+++ fairy/text_emotion.py
@@ -1,11 +1,20 @@
 import re
+import config
 
 class TextEmotionAnalyzer:
     def __init__(self):
-        pass
+        self.sad_keywords = getattr(config, 'SAD_KEYWORDS', [])
+        self.happy_keywords = getattr(config, 'HAPPY_KEYWORDS', [])
+        self.slang_map = getattr(config, 'SLANG_MAP', {})
 
     def _remove_whitespace(self,text):
         return "".join(text.split())
     
     def _normalize_repeated_chars(self,text):
         return re.sub(r'(.)\1{2,}',r'\1\1',text)
+    
+    def _censor_slang(self, text):
+        processed_text = text.lower()
+        for slang, replacement in self.slang_map.items():
+            processed_text = processed_text.replace(slang, replacement)
+        return processed_text
