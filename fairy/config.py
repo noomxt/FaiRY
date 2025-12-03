44deaeb0f6d45c8a5b0482cc1e35d9bd92e45a77
mihyeon05 <algus0251@naver.com>
2025-12-03 11:33:07 +0900

    요청대로 data 폴더 경로로 수정

--- fairy/config.py
+++ fairy/config.py
@@ -6,15 +6,17 @@
 """
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(CURRENT_DIR)
+DATA_DIR = os.path.join(BASE_DIR, 'data')
 
 EMOTION_FILES = {
-    "기쁨": os.path.join(BASE_DIR, 'data_joy.csv'),
-    "슬픔": os.path.join(BASE_DIR, 'data_sad.csv'),
-    "분노": os.path.join(BASE_DIR, 'data_anger.csv'),
-    "피곤함": os.path.join(BASE_DIR, 'data_tired.csv'),
-    "무서움": os.path.join(BASE_DIR, 'data_fear.csv'),
-    "평온": os.path.join(BASE_DIR, 'data_peace.csv')
+    "기쁨": os.path.join(DATA_DIR, 'data_joy.csv'),
+    "슬픔": os.path.join(DATA_DIR, 'data_sad.csv'),
+    "분노": os.path.join(DATA_DIR, 'data_anger.csv'),
+    "피곤함": os.path.join(DATA_DIR, 'data_tired.csv'),
+    "무서움": os.path.join(DATA_DIR, 'data_fear.csv'),
+    "평온": os.path.join(DATA_DIR, 'data_peace.csv')
 }
 
 EMOTIONS = list(EMOTION_FILES.keys())
