d0f2097eb33b715da1f42bd8053d3a30870230c3
baesohyun <baesohyun04@hanyang.ac.kr>
2025-12-10 01:08:06 +0900

    __init__.py 내용 추가

--- fairy/__init__.py
+++ fairy/__init__.py
@@ -1,1 +1,6 @@
+from .text_emotion import TextEmotionAnalyzer
+from .image_emotion import EmotionAnalyzer
+from .config import EMOTIONS, EMOTION_FILES
 
+__version__ = '1.0.0'
+__author__ = 'AIF5 Team (Sohyeon, Mihyeon, Jisoo, Dayeon, Minju)'
